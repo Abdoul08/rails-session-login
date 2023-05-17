d3d8f8eae0b5cdcf73ac40efb8d8a4a559a8865a
Abdoul08 <diawarabdoulaye55@gmail.com>
2023-05-17 20:10:48 +0200

    session login task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
     private
     def login_required
         redirect_to new_session_path unless current_user
+        flash[:notice] = "Veuillez vous connecter"
     end
 end
