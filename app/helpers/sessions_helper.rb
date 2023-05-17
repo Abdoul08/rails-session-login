2f05c4b4c4c3f33045a84cd46693be06ad73f7cc
Abdoul08 <diawarabdoulaye55@gmail.com>
2023-05-17 16:03:05 +0200

    rails session login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,9 @@ module SessionsHelper
         current_user.present?
     end
 
-    
+    def log_in(user)
+        session[:user_id] = user.id
+    end
 
     def current_user?(user)
         user == current_user
